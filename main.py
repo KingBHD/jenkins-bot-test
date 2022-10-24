3b3e54d1551e7901fbfb3e09147a26c6ab1d66fb
BHBHD <BHBHD65@gmail.com>
2022-10-24 23:56:19 +0530

    Added base files

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import discord
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+CLIENT_TOKEN = os.getenv("CLIENT_TOKEN")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(CLIENT_TOKEN)
